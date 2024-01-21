1239c95637873340e81021f9e522c742dc563b25
sneha <sneha@Snehal>
2024-01-21 16:01:43 +0530

    fourth commit

---
+++ Operator/src/com/edubridge/operator/RelationalOperator.java
@@ -0,0 +1,20 @@
+package com.edubridge.operator;
+
+import java.util.Scanner;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		
+
+
+	}
+
+}
