e2b7a8d4fc996e66a376bae2dbac33c25fe4a1e6
sneha <sneha@Snehal>
2024-01-21 15:57:40 +0530

    Third commit

---
+++ Operator/src/com/edubridge/operator/ArthmeticOperator.java
@@ -0,0 +1,19 @@
+package com.edubridge.operator;
+
+import java.util.Scanner;
+
+public class ArthmeticOperator {
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+
+	}
+
+}
